8646a5a111993dc58c322d3c6154b2d6197fdb06
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2018-08-29 23:53:33 +0700

    Add description from README to pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='flask_flaskwork',
@@ -9,6 +15,8 @@ setup(
     author_email='tim.is@self-proclaimed.ninja',
     py_modules=['flask_flaskwork'],
     zip_safe=False,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         'Flask', 'sqlalchemy', 'sqlparse'
     ]
